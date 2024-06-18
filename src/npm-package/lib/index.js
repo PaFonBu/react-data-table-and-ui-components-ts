52ad377ae802b17c2c498c943d11509338609fd3
PabloFB <pafonbu@gmail.com>
2024-06-18 17:14:58 -0400

    .gitignore fix

--- src/npm-package/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-var helloWorld = function () { return "hello world"; };
-exports.helloWorld = helloWorld;
